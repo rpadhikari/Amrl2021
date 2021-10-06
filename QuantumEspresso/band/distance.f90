d2be50bebb94c1462943af63b3c8a06acebdf6a7
Rajendra Adhikari <rpadhikari@gmail.com>
2021-10-06 13:40:15 +0545

    Update distance.f90

--- QuantumEspresso/band/distance.f90
+++ QuantumEspresso/band/distance.f90
@@ -4,6 +4,6 @@ function distance(a, b)
   integer(2) :: i
   integer(2), parameter :: n=3
   real(8) a(n), b(n)
-  distance=sqrt((b(1)-a(i))**2 + (b(2)-a(2))**2 + (b(3)-a(3))**2)
+  distance=sqrt((b(1)-a(1))**2 + (b(2)-a(2))**2 + (b(3)-a(3))**2)
 end function distance
 
