aefea04ad6f0abfdc442cf0caefdbaca21c1de0c
Rajendra Adhikari <rpadhikari@gmail.com>
2022-03-20 16:06:57 +0545

    Update volume.sh

--- QuantumEspresso/volume/volume.sh
+++ QuantumEspresso/volume/volume.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This bash script will optimize the volume of Hexagonal crystals scanning 
-# from -3 to +3 % of experimental volume. 
+# This bash script will optimize the volume of Hexagonal crystals scanning
+# from -3 to +3 % of experimental volume.
 # For testing case a=b=3.2162899971 and c=5.2399621010
 # Written by: Rajendra Adhikari, Department of Physics,
 # Kathmandu University, Kavre, Nepal
@@ -8,21 +8,21 @@
 # please do NOT remove above copyright notice.
 
 rm -rf etot.dat stress.dat
-a0 = 3.2162899971  # alat  1.0000000  0.000000000   0.00000
-b0 = 3.2162899971  # blat -0.5000000  0.866025404   0.00000
-c0 = 5.2399621010  # clat  0.0000000  0.000000000   1.63000
+a0=3.2162899971  # alat  1.0000000  0.000000000   0.00000
+b0=3.2162899971  # blat -0.5000000  0.866025404   0.00000
+c0=5.2399621010  # clat  0.0000000  0.000000000   1.63000
 pi=3.14159265358979323846
-angle=(2.0/3.0)*pi
+angle=`echo "(2.0/3.0)*$pi" | bc -l`
 for m in {0..24}
   do
-    alat=`echo "a0*(97.0/100.0+$m*0.0025)" | bc -l`
-    clat=`echo "c0*(97.0/100.0+$m*0.0025)" | bc -l`
-    vol=`echo "alat*alat*clat*s(angle)" | bc -l`
+    alat=`echo "$a0*(97.0/100.0+$m*0.0025)" | bc -l`
+    clat=`echo "$c0*(97.0/100.0+$m*0.0025)" | bc -l`
+    vol=`echo "$alat*$alat*$clat*s($angle)" | bc -l`
 cat > scf.in << EOF
 &control
     calculation='scf'
     restart_mode='from_scratch'
-    pseudo_dir = '../pseudo'
+    pseudo_dir = '/home/raja/pseudo'
     outdir='./'
     disk_io='none'
     tstress=.true.
@@ -30,7 +30,8 @@ cat > scf.in << EOF
 /
 &system
     ibrav=4
-    celldm(1)=$alat
+    a = $alat
+    c = $clat
     nat=4
     ntyp=2
     ecutwfc=80.0
@@ -47,18 +48,20 @@ ATOMIC_POSITIONS {crystal}
   Ga  0.333333333     0.666666667     0.999119997
   Ga  0.666666667     0.333333333     0.499119997
   N   0.333333333     0.666666667     0.375880003
-  V   0.666666667     0.333333333     0.875880003
+  N   0.666666667     0.333333333     0.875880003
 K_POINTS {automatic}
  8 8 6 0 0 0
 EOF
-mpirun -np 12 pw.x -nk 2 -npw 6 -inp scf.in > scf.out
+#mpirun -np 12 pw.x -nk 2 -npw 6 -inp scf.in > scf.out
+pw.x < scf.in > scf.out
 te=`grep ! scf.out | tail -1 | awk '{print $5}'`
-ft=`grep 'l fo' scf.out|awk '{print $4}'`
-sxx=`grep -A1 'l   s' scf.out|tail -1|awk '{print $4}'`
-echo "$alat  $sxx" >> stress.dat
-echo "$alat clat $te" >> etot.dat
-echo "$vol  $te" >> etot.dat
-echo "$alat  $ft" >> force.dat
+#ft=`grep 'l fo' scf.out|awk '{print $4}'`
+#sxx=`grep -A1 'l   s' scf.out|tail -1|awk '{print $4}'`
+#echo "$alat  $sxx" >> stress.dat
+echo "$alat $clat $te" >> etot.dat
+echo "$vol $te" >> volume.dat
+#echo "$alat  $ft" >> force.dat
 mv scf.in scf$m.in
 mv scf.out scf$m.out
 done
+
